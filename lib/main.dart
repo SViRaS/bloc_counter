2524c2cb20c7d822616c16fe9f31667b9cb31afa
SViRa <s.sviridenko05@bk.ru>
2022-09-13 23:44:25 +0300

    add multi provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:counter_bloc/bloc.dart';
+import 'package:counter_bloc/counter_bloc.dart';
+import 'package:counter_bloc/user_bloc/bloc/user_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -9,23 +10,9 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -33,16 +20,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -50,40 +27,72 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+  final counter_bloc = CounterBloc()..add(CounterDel());
+  final user_bloc = UserBloc();
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<CounterBloc>(
-      create: (context) => CounterBloc(),
-      child: BlocBuilder<CounterBloc, int>(
-        builder: (context, state) => Scaffold(
-          floatingActionButton: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterBloc>(context).add(CounterAdd());
-                  },
-                  icon: Icon(Icons.add)),
-              IconButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterBloc>(context).add(CounterDel());
-                  },
-                  icon: Icon(Icons.delete))
-            ],
-          ),
-          body: Center(
-            child: Text(
-              state.toString(),
-              style: TextStyle(fontSize: 50, color: Colors.black),
-            ),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<CounterBloc>(
+          create: (context) => CounterBloc(),
+        ),
+        BlocProvider<UserBloc>(
+          create: (context) => UserBloc(),
+        ),
+      ],
+      child: Scaffold(
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+                onPressed: () {
+                  counter_bloc.add(CounterAdd());
+                },
+                icon: Icon(Icons.add)),
+            IconButton(
+                onPressed: () {
+                  counter_bloc.add(CounterDel());
+                },
+                icon: Icon(Icons.delete)),
+            IconButton(
+                onPressed: () {
+                  user_bloc.add(UserGetUsersEvent(counter_bloc.state));
+                },
+                icon: Icon(Icons.person)),
+                IconButton(
+                onPressed: () {
+                  user_bloc.add(UserGetUsersJobEvent(counter_bloc.state));
+                },
+                icon: Icon(Icons.work)),
+          ],
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Column(children: [
+              BlocBuilder<CounterBloc, int>(
+                bloc: counter_bloc,
+                builder: (context, state) => Text(
+                  state.toString(),
+                  style: TextStyle(fontSize: 50, color: Colors.black),
+                ),
+              ),
+              BlocBuilder<UserBloc, UserState>(
+                  bloc: user_bloc,
+                  builder: (context, state) {
+                    final users = state.users;
+                    final job = state.job;
+                    return Column(
+                      children: [
+                        if (state.isLoading)
+                          CircularProgressIndicator(),
+                        if (users.isNotEmpty)
+                          ...users.map((e) => Text(e.name)),
+                          if (job.isNotEmpty)
+                          ...job.map((e) => Text(e.name)),
+                      ],
+                    );
+                  }),
+            ]),
           ),
         ),
       ),
